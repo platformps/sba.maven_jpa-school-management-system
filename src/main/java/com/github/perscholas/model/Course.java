6269d26692bf9b83d9596e657ffd2437bc38cfb3
issagodinez <lauragodinez45@gmail.com>
2020-09-03 12:38:30 -0400

    Part 1 Complete

--- src/main/java/com/github/perscholas/model/Course.java
+++ src/main/java/com/github/perscholas/model/Course.java
@@ -1,5 +1,9 @@
 package com.github.perscholas.model;
+import javax.persistence.*;
+import java.util.*;
 
 // TODO - Annotate and Implement respective interface and define behaviors
 public class Course {
+
+
 }
---
+++ src/main/resources/students.create-table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE student (
+email varchar(50) not null primary key
+name varchar(50) not null
+password varchar(50) not null
+);
