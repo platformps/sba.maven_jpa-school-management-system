8fd7bfe9dee541f8ff9b9d9686a6b6b41025d99d
lyoumbi <nkyoli@gmail.com>
2020-09-03 23:56:22 -0400

    Done

---
+++ src/main/resources/courses.create-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Course (
+  id int not null,
+  name varchar(50) NOT NULL,
+  instructor varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
--- src/test/java/com/github/perscholas/service/studentservice/GetAllStudentsTest.java
+++ src/test/java/com/github/perscholas/service/studentservice/GetAllStudentsTest.java
@@ -1,13 +1,19 @@
 package com.github.perscholas.service.studentservice;
 
+import com.github.perscholas.DatabaseConnection;
 import com.github.perscholas.JdbcConfigurator;
 import com.github.perscholas.dao.StudentDao;
+import com.github.perscholas.model.Student;
 import com.github.perscholas.model.StudentInterface;
 import com.github.perscholas.service.StudentService;
 import com.github.perscholas.utils.DirectoryReference;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.io.File;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,7 +21,7 @@ import java.util.List;
  * @created 02/12/2020 - 8:22 PM
  */
 public class GetAllStudentsTest {
-    @Before // TODO (OPTIONAL) - Use files to execute SQL commands
+    @Before
     public void setup() {
         DirectoryReference directoryReference = DirectoryReference.RESOURCE_DIRECTORY;
         File coursesSchemaFile = directoryReference.getFileFromDirectory("courses.create-table.sql");
@@ -31,15 +37,29 @@ public class GetAllStudentsTest {
     }
 
     // given
-    // TODO - Add `@Test` annotation
+    @Test
     public void test() {
         JdbcConfigurator.initialize();
-        StudentDao service = (StudentDao) new StudentService();
+        StudentDao service = new StudentService();
 
         // when
         List<StudentInterface> studentList = service.getAllStudents();
 
         // then
-        // TODO - define _then_ clause
+        ResultSet resultSet = DatabaseConnection.MANAGEMENT_SYSTEM.executeQuery("SELECT * FROM student");
+        List<StudentInterface> expectedStudents = new ArrayList<>();
+        try {
+            while(resultSet.next()) {
+                Student student = new Student();
+                student.setEmail(resultSet.getString("email"));
+                student.setName(resultSet.getString("name"));
+                student.setPassword(resultSet.getString("password"));
+                expectedStudents.add(student);
+            }
+        } catch(Exception e) {
+            throw new Error(e);
+        }
+        Assert.assertArrayEquals(studentList.toArray(), expectedStudents.toArray());
     }
+
 }
