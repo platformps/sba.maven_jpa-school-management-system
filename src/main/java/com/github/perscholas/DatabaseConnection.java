58004626c788024f03ab8c2635cb44990bfbc5ca
unknown <dyirchott@hotmail.com>
2020-09-07 13:13:11 -0400

    first commit

--- src/main/java/com/github/perscholas/DatabaseConnection.java
+++ src/main/java/com/github/perscholas/DatabaseConnection.java
@@ -5,37 +5,42 @@ import com.github.perscholas.utils.IOConsole;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * Created by leon on 2/18/2020.
  */
 public enum DatabaseConnection implements DatabaseConnectionInterface {
     MANAGEMENT_SYSTEM,
-    UAT;
+    UAT;   ///?
 
     private static final IOConsole console = new IOConsole(IOConsole.AnsiColor.CYAN);
     private final ConnectionBuilder connectionBuilder;
 
     DatabaseConnection(ConnectionBuilder connectionBuilder) {
+
         this.connectionBuilder = connectionBuilder;
     }
 
     DatabaseConnection() {
         this(new ConnectionBuilder()
                 .setUser("root")
-                .setPassword("")
+                .setPassword("Morning42Root")
                 .setPort(3306)
-                .setDatabaseVendor("mariadb")
+                .setDatabaseVendor("mysql")
                 .setHost("127.0.0.1"));
     }
 
     @Override
     public String getDatabaseName() {
+
         return name().toLowerCase();
     }
 
     @Override
     public Connection getDatabaseConnection() {
+
         return connectionBuilder
                 .setDatabaseName(getDatabaseName())
                 .build();
@@ -43,15 +48,17 @@ public enum DatabaseConnection implements DatabaseConnectionInterface {
 
     @Override
     public Connection getDatabaseEngineConnection() {
+
         return connectionBuilder.build();
     }
 
     @Override
     public void create() {
-        String sqlStatement = null; // TODO - define statement
+        String sqlStatement = "CREATE DATABASE IF NOT EXISTS " + this.getDatabaseName(); // TODO - define statement
         String info;
         try {
             // TODO - execute statement
+            executeStatement(sqlStatement);
             info = "Successfully executed statement `%s`.";
         } catch (Exception sqlException) {
             info = "Failed to executed statement `%s`.";
@@ -61,18 +68,58 @@ public enum DatabaseConnection implements DatabaseConnectionInterface {
 
     @Override
     public void drop() {
+        String sqlStatement = "DROP SCHEMA IF EXISTS " + this.getDatabaseName(); // TODO - define statement
+        String info;
+        try {
+            // TODO - execute statement
+            executeStatement(sqlStatement);
+            info = "Successfully executed statement `%s`.";
+        } catch (Exception sqlException) {
+            info = "Failed to executed statement `%s`.";
+        }
+        console.println(info, sqlStatement);
     }
 
     @Override
     public void use() {
+        String sqlStatement = "USE " + this.getDatabaseName(); // TODO - define statement
+        String info;
+        try {
+            // TODO - execute statement
+            executeStatement(sqlStatement);
+            info = "Successfully executed statement `%s`.";
+        } catch (Exception sqlException) {
+            info = "Failed to executed statement `%s`.";
+        }
+        console.println(info, sqlStatement);
     }
 
     @Override
     public void executeStatement(String sqlStatement) {
+        String info;
+        try {
+            // TODO - execute statement
+            getDatabaseEngineConnection().createStatement().execute(sqlStatement);
+
+            info = "Successfully executed statement `%s`.";
+        } catch (SQLException e) {
+            info = "Failed to executed statement `%s`.";
+
+            console.println(info, sqlStatement);
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public ResultSet executeQuery(String sqlQuery) {
-        return null;
+        String info;
+        try {
+            // TODO - execute statement
+            return getDatabaseConnection().createStatement().executeQuery(sqlQuery);
+        } catch (SQLException e) {
+            info = "Failed to executed query `%s`.";
+            console.println(info, sqlQuery);
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/github/perscholas/JdbcConfigurator.java
+++ src/main/java/com/github/perscholas/JdbcConfigurator.java
@@ -3,12 +3,14 @@ package com.github.perscholas;
 import com.github.perscholas.utils.DirectoryReference;
 import com.github.perscholas.utils.FileReader;
 
+
 import java.io.File;
 
 public class JdbcConfigurator {
     static {
         try {
             // TODO - Attempt to register JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (Exception e) {
             throw new Error(e);
         }
