6927a929e51de7d62c838d93a4869c75571e6b01
David_Kyle_Burchett <davidkburchett@gmail.com>
2020-09-07 14:48:20 -0400

    part 2 done

--- src/main/java/com/github/perscholas/model/Course.java
+++ src/main/java/com/github/perscholas/model/Course.java
@@ -1,5 +1,58 @@
 package com.github.perscholas.model;
 
-// TODO - Annotate and Implement respective interface and define behaviors
-public class Course {
+import javax.persistence.*;
+
+@Entity
+@Table(name = "course")
+public class Course implements CourseInterface{
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "instructor", nullable = false, length = 50)
+    private String instructor;
+
+    public Course() {
+    }
+
+    public Course(int id, String name, String instructor) {
+        this.id = id;
+        this.name = name;
+        this.instructor = instructor;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getInstructor() {
+        return instructor;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
 }
--- src/main/java/com/github/perscholas/model/Student.java
+++ src/main/java/com/github/perscholas/model/Student.java
@@ -1,5 +1,58 @@
 package com.github.perscholas.model;
 
-// TODO - Annotate and Implement respective interface and define behaviors
-public class Student {
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student implements StudentInterface{
+
+    @Id
+    @Column(name = "email", nullable = false, length = 50)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String email;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "password", nullable = false, length = 50)
+    private String password;
+
+    public Student() {
+    }
+
+    public Student(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
