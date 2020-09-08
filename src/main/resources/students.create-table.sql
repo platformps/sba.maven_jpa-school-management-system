f21e17fb6e74710f38ad11e03dc3f251b59c689e
Steven Snyder <sts12589@gmail.com>
2020-09-08 16:42:28 -0400

    databse connectivity working and populating db

--- src/main/resources/students.create-table.sql
+++ src/main/resources/students.create-table.sql
@@ -1,8 +1,8 @@
 use management_system;
 
-DROP TABLE IF EXISTS Students;
+DROP TABLE IF EXISTS Student;
 
-CREATE TABLE Students(
+CREATE TABLE Student(
     email varchar(50) not null,
     name varchar(50) not null,
     password varchar(50) not null,
