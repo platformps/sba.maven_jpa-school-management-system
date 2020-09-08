2e3055b38f04c26557cbdce4c4fb7ef1b4e30976
Ezra Vance <3zravance@gmail.com>
2020-09-08 10:33:48 -0400

    sql create table statments added.

---
+++ src/main/resources/courses.create-table.sql
@@ -0,0 +1,5 @@
+create table Course (
+id int not null (PK) /*Unique course identifier*/,
+name varchar(50) not null /*Provides the name of the course*/,
+instructor varchar(50) not null /*Provides the name of the instructor*/
+);
