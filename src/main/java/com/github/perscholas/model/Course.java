feda6a18c5be90601ad714b89dd72e2df154ed0e
Emmanuel Orubele <blontf@gmail.com>
2020-09-08 09:02:35 -0400

    JPA

--- src/main/java/com/github/perscholas/model/Course.java
+++ src/main/java/com/github/perscholas/model/Course.java
@@ -1,5 +1,50 @@
 package com.github.perscholas.model;
 
+import javax.persistence.*;
+
 // TODO - Annotate and Implement respective interface and define behaviors
-public class Course {
+@Entity
+@Table(name = "course")
+public class Course implements CourseInterface {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "instructor")
+    private String instructor;
+
+
+    @Override
+    public Integer getId() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getInstructor() {
+        return null;
+    }
+
+    @Override
+    public void setId(Integer id) {
+
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public void setInstructor(String instructor) {
+
+    }
 }
--- src/main/java/com/github/perscholas/model/Student.java
+++ src/main/java/com/github/perscholas/model/Student.java
@@ -1,5 +1,55 @@
 package com.github.perscholas.model;
 
 // TODO - Annotate and Implement respective interface and define behaviors
-public class Student {
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student implements StudentInterface {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+
+    @Override
+    public String getEmail() {
+        return this.email;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
