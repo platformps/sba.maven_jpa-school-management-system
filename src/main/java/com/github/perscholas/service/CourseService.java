0c5c54b1b57e29bd5a81eabe115f4bd67338c9c3
David Frary <fraryd@gmail.com>
2020-09-07 22:46:04 -0400

    finished edits on 'CourseService.java'

--- src/main/java/com/github/perscholas/service/CourseService.java
+++ src/main/java/com/github/perscholas/service/CourseService.java
@@ -1,12 +1,50 @@
 package com.github.perscholas.service;
 
+import com.github.perscholas.DatabaseConnection;
+import com.github.perscholas.dao.CourseDao;
+import com.github.perscholas.dao.StudentDao;
+import com.github.perscholas.model.*;
+import com.github.perscholas.model.Course;
+import com.github.perscholas.model.CourseInterface;
+import com.github.perscholas.model.Student;
+import com.github.perscholas.model.StudentInterface;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 // TODO - Implement respective DAO interface
-public class CourseService {
-    import com.github.perscholas.DatabaseConnection;
-    import com.github.perscholas.dao.CourseDao;
-    import com.github.perscholas.dao.StudentDao;
-    import com.github.perscholas.model.*;
-    import java.sql.ResultSet;
-    import java.util.ArrayList;
-    import java.util.List;
+public class CourseService  implements CourseDao {
+
+    private final DatabaseConnection dbc;
+
+    public CourseService(DatabaseConnection dbc){
+        this.dbc = dbc;
+    }
+
+    public CourseService() {
+        this(DatabaseConnection.UAT);
+    }
+
+    @Override
+    public List<CourseInterface> getAllCourses() {
+        ResultSet resultset = dbc.executeQuery("SELECT * FROM Course");
+        try{
+            List<CourseInterface> courseList = new ArrayList<CourseInterface>();
+            while(resultSet.next()) {
+                CourseInterface course = new Course();
+                course.setID(resultSet.getInt("name"));
+                course.setName(resultSet.getString("name"));
+                course.setInstructor(resultSet.getString("instructor"));
+                courseList.add(course);
+            }
+            return courseList;
+        } catch(Exception e) {
+            throw new Error(e);
+        }
+    }
+
 }
