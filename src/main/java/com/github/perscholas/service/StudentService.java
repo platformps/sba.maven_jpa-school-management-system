1a0ac8f726104d4ebe4b2c60e4270f1847e6032e
David Frary <fraryd@gmail.com>
2020-09-07 23:03:00 -0400

    finished edits on 'StudentService.java'

--- src/main/java/com/github/perscholas/service/StudentService.java
+++ src/main/java/com/github/perscholas/service/StudentService.java
@@ -3,12 +3,15 @@ import com.github.perscholas.model.*;
 
 import com.github.perscholas.DatabaseConnection;
 import com.github.perscholas.dao.StudentDao;
+import com.github.perscholas.model.Course;
 import com.github.perscholas.model.CourseInterface;
 import com.github.perscholas.model.Student;
 import com.github.perscholas.model.StudentInterface;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityContraintViolationException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,7 +31,15 @@ public class StudentService implements StudentDao {
     public List<StudentInterface> getAllStudents() {
         ResultSet resultSet = dbc.executeQuery("SELECT * FROM students");
         try {
-            return null; // TODO - Parse `List<StudentInterface>` from `resultSet`
+            List<StudentInterface> studentList = new ArrayList<StudentInterface>();
+            while(resultSet.next()){
+                StudentInterface student = new Student();
+                student.setEmail(resultSet.getString("email"));
+                student.setName(resultSet.getString("name"));
+                student.setPassword(resultSet.getString("password"));
+                studentList.add(student);
+            }
+            return studentList; // TODO - Parse `List<StudentInterface>` from `resultSet`
         } catch(Exception e) {
             throw new Error(e);
         }
@@ -36,17 +47,41 @@ public class StudentService implements StudentDao {
 
     @Override
     public StudentInterface getStudentByEmail(String studentEmail) {
-        return null;
+        ResultSet resultSet = dbc.executeQuery
+                ("SELECT * FROM Student WHERE email = \'" + studentEmail + "\'");
+        try{
+            resultSet.next();
+            StudentInterface student = new Student();
+            student.setEmail(resultSet.getString("email"));
+            student.setName(resultSet.getString("name"));
+            student.setPassword(resultSet.getString("password"));
+
+            return student;
+        } catch(Exception e) {
+            throw new Error(e);
+        }
     }
 
     @Override
     public Boolean validateStudent(String studentEmail, String password) {
-        return null;
+        String query = "SELECT * FROM Student WHERE email = \'" +
+                studentEmail + "\'" +
+                "AND password = \'" +
+                password + "\';";
+        ResultSet resultSet = dbc.executeQuery(query);
+        try {
+            return resultSet.next();
+        } catch(Exception e) {
+            throw new Error(e);
+        }
     }
 
     @Override
     public void registerStudentToCourse(String studentEmail, int courseId) {
-
+    String query = "INSERT INTO StudentCourse (studentEmail, courseID) values (" +
+            "\'" + studentEmail + "\'," +
+            "\'" + courseId + "\');";
+    dbc.executeStatement(query);
     }
 
     @Override
